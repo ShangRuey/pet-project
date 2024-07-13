3304db66a20e322c055688030889549c87d1faf0
ShangRuey <tom830522@gmail.com>
2024-07-13 22:17:22 +0800

    feat:針對login 頁面，新增 模擬登入成功 取得authkey 並儲存於Cookie

--- js/login.js
+++ js/login.js
@@ -1,8 +1,29 @@
 const loginForm = document.querySelector('.login-form');
+const userName = document.querySelector('#username');
+const password = document.querySelector('#password');
 
-//登入成功、失敗 todo
+//模擬使用者資料
+const user = {
+  account: 'test',
+  password: 'test'
+}
+
+console.log(user)
+//模擬登入提交功能
 loginForm.addEventListener('submit', (event) =>{
   event.preventDefault();
-  console.log("登入成功")
-  console.log("登入失敗")
+  const enterAccount = userName.value.trim();
+  const enterPassword = password.value.trim();
+  if(user.account === enterAccount && user.password === enterPassword){
+    console.log("登入成功")
+    /* 模擬擁有authkey 並儲存於Cookie
+    const authkey = 'testAuth'
+    setCookie('authkey', authkey)
+    */
+    //跳轉回前網頁
+    window.history.back();    
+  }
+  else{
+    console.log("登入失敗")
+  }
 })
